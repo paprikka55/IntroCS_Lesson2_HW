4f5503f406a361db9bc67547cf92c8aa3fc88c94
Alexander Malyatin <paprikka84@gmail.com>
2024-02-20 10:59:12 +0600

    Added HW:Task1, Task2

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 7 == 0 && num % 23 == 0)
+{
+  Console.WriteLine("да");
+}
+else
+{
+  Console.WriteLine("нет");
+}
--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int xpoint = new int();
+int ypoint = new int();
+bool exit = new bool();
+Console.WriteLine("Введите координаты точек X и Y. Координаты не должны быть равны 0!");
+while (true)
+{
+  exit = true;
+  Console.Write("Введите X (целое число): ");
+  try
+  {
+    xpoint = Convert.ToInt32(Console.ReadLine());
+  }
+  catch
+  {
+    Console.WriteLine("Неверный формат ввода данных!");
+    exit = false;
+  }
+  if (exit == true)
+  { break; }
+}
+
+while (true)
+{
+  exit = true;
+  Console.Write("Введите Y (целое число): ");
+  try
+  {
+    ypoint = Convert.ToInt32(Console.ReadLine());
+  }
+  catch
+  {
+    Console.WriteLine("Неверный формат ввода данных!");
+    exit = false;
+  }
+  if (exit == true)
+  { break; }
+}
+
+if (xpoint == 0 || ypoint == 0)
+{
+  Console.WriteLine($"Введенные координаты точек не должны быть равны 0! Вы ввели X: {xpoint}, Y: {ypoint}.");
+}
+else
+{
+  if (xpoint > 0 && ypoint > 0)
+  {
+    Console.WriteLine($"Координаты X: {xpoint} и Y: {ypoint} принадлежат 1 четверти.");
+  }
+  else if (xpoint < 0 && ypoint > 0)
+  {
+    Console.WriteLine($"Координаты X: {xpoint} и Y: {ypoint} принадлежат 2 четверти.");
+  }
+  else if (xpoint < 0 && ypoint < 0)
+  {
+    Console.WriteLine($"Координаты X: {xpoint} и Y: {ypoint} принадлежат 3 четверти.");
+  }
+  else
+  {
+    Console.WriteLine($"Координаты X: {xpoint} и Y: {ypoint} принадлежат 4 четверти.");
+  }
+}
+
+
