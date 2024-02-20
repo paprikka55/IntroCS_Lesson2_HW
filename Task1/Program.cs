bcb29e5d02d334e0f67b614fddff1032bf6c54af
Alexander Malyatin <paprikka84@gmail.com>
2024-02-20 11:29:17 +0600

    Added HW:task3

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,10 +1,25 @@
-﻿Console.Write("Введите целое число: ");
-int num = Convert.ToInt32(Console.ReadLine());
+﻿bool exit = new bool();
+int num = new int();
+while (true)
+{
+  Console.Write("Введите целое число: ");
+  exit = true;
+  try
+  { num = Convert.ToInt32(Console.ReadLine()); }
+  catch
+  {
+    Console.WriteLine("Неверный формат ввода данных!");
+    exit = false;
+  }
+  if (exit == true)
+  { break; }
+}
+
 if (num % 7 == 0 && num % 23 == 0)
 {
-  Console.WriteLine("да");
+  Console.WriteLine($"Число {num} кратно одновременно 7 и 23.");
 }
 else
 {
-  Console.WriteLine("нет");
+  Console.WriteLine($"Число {num} не кратно одновременно 7 и 23.");
 }
--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool exit = new bool();
+int num = new int();
+while (true)
+{
+  Console.Write("Введите целое число: ");
+  exit = true;
+  try
+  { num = Convert.ToInt32(Console.ReadLine()); }
+  catch
+  {
+    Console.WriteLine("Неверный формат ввода данных!");
+    exit = false;
+    num = 0;
+  }
+  if (num >= 10 && num <= 99)
+  { exit = true; }
+  else
+  {
+    if (exit == true)
+    { Console.WriteLine($"Нужно ввести число в диапазоне от 10 до 99! Вы ввели {num}."); }
+    exit = false;
+  }
+  if (exit == true)
+  { break; }
+}
+if (num % 10 > num / 10)
+{ Console.WriteLine($"Наибольшая цифра числа {num}: {num % 10}"); }
+else
+{ Console.WriteLine($"Наибольшая цифра числа {num}: {num / 10}"); }
